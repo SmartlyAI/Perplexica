1df9df4f808e12079db5b24926efa00a49a13510
elqessouartariq <tariq@smartly.ai>
2025-02-05 20:25:41 +0100

    add share dialog and button

--- ui/components/Navbar.tsx
+++ ui/components/Navbar.tsx
@@ -2,11 +2,13 @@ import { PanelLeftOpen, PanelRightOpen, Settings } from 'lucide-react';
 import { useState } from 'react';
 import useSidebarStore from '@/stores/global-stores';
 import SettingsDialog from './SettingsDialog';
+import ShareButton from './ShareButton';
 
 const Navbar = () => {
   const { toggleSidebar } = useSidebarStore();
   const { isSidebarOpen } = useSidebarStore();
   const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+  const [isShareOpen, setIsShareOpen] = useState(false);
 
   return (
     <div className="fixed top-0 z-40 flex justify-between p-5 items-center w-full bg-[#ffffff] dark:bg-dark-secondary">
@@ -14,10 +16,13 @@ const Navbar = () => {
 
       <SettingsDialog isOpen={isSettingsOpen} setIsOpen={setIsSettingsOpen} />
 
-      <Settings
-        className="cursor-pointer"
-        onClick={() => setIsSettingsOpen(true)}
-      />
+      <div className="flex items-center space-x-5">
+        <ShareButton isOpen={isShareOpen} setIsOpen={setIsShareOpen} />
+        <Settings
+          className="cursor-pointer"
+          onClick={() => setIsSettingsOpen(true)}
+        />
+      </div>
 
     </div>
   );
